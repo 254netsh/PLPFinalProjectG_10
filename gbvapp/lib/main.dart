048ee4d35a91885ef795229517ff31ff8557716a
David Nanjila <nanjiladavid2@gmail.com>
2024-06-06 09:35:36 +0300

    Separate the homepage code from the main.dart file

--- gbvapp/lib/main.dart
+++ gbvapp/lib/main.dart
@@ -1,7 +1,7 @@
 // main.dart
 import 'package:flutter/material.dart';
 import 'about_us.dart'; // Import the AboutUsPage
-
+import 'myhomepage.dart'; // Import the MyHomePage
 void main() {
   runApp(MyApp());
 }
@@ -19,51 +19,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-  static final List<Widget> _widgetOptions = <Widget>[
-    const Text('Home Page Content'), // Placeholder for the Home Page
-    AboutUsPage(), // The About Us Page
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('She Advocates'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info),
-            label: 'About Us',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color.fromARGB(255, 233, 135, 212),
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-}
